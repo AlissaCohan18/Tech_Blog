fd90b5d61b332079c1cc1362f16db5601baf641c
Alissa Cohan <alissacohan18@gmail.com>
2023-02-17 10:49:08 -0500

    moved path at top of declarations

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const path = require('path');
 const session = require('express-session');
 
 const app = express();
@@ -33,7 +33,7 @@ app.set('view engine', 'handlebars');
 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers'));
